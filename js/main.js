b759e03698ac834dc7b556c66d67e0c031486904
Gregory Shuford <bearshuford@me.com>
2016-09-23 15:42:19 -0400

    added background color update, improved hover method

--- js/main.js
+++ js/main.js
@@ -1,126 +1,65 @@
 (function(){
   "use strict";
 
-  var hexInterval;
+  var isHex = false;
 
-  var timer   = document.querySelector('.elapsed-time');
+  var timer   = document.querySelector('.clock');
 
-  var hours   = document.querySelector('.elapsed-time .hours');
-  var minutes = document.querySelector('.elapsed-time .minutes');
-  var seconds = document.querySelector('.elapsed-time .seconds');
+  var hours   = document.querySelector('.clock .hours');
+  var minutes = document.querySelector('.clock .minutes');
+  var seconds = document.querySelector('.clock .seconds');
 
   var startTime = new Date();
 
   console.log("startTime: ", startTime);
 
-  var timerInterval = window.setInterval(logElapsedTime, 1000);
+  var clockInterval = window.setInterval(logTime, 500);
 
+  function showHex()     {isHex = true;}
+  function showDecimal() {isHex = false;}
 
-
-  function toHex(num) {
-     var x = parseInt(num,10);
-     x = x.toString(16);
-     if(x.length == 1)  {x = "0" + x;}
-     return x;
-  }
-
-
-  function millisecondsToInterval(ms) {
-     var msLeft = ms;
-
-     var msInSecond  = 1000;
-     var msInMinute  = 60 * msInSecond;
-     var msInHour    = 60 * msInMinute;
-
-     var hourCount = Math.floor(msLeft / msInHour);
-     msLeft = msLeft % msInHour;
-
-     var minuteCount =  Math.floor(msLeft / msInMinute);
-     msLeft = msLeft % msInMinute;
-
-     var secondCount =  Math.floor(msLeft / msInSecond);
-
-     return [hourCount, minuteCount, secondCount];
-  }
-
-  function displayInterval(interval) {
-     hours.textContent   = ("0" + interval[0]).slice(-2);
-     minutes.textContent = ("0" + interval[1]).slice(-2);
-     seconds.textContent = ("0" + interval[2]).slice(-2);
- }
-
- function updateBackground(interval) {
-
-
-   // document.body.style.background = colorHex;
-
-}
-
-   function displayHex(interval){
-      hours.textContent   = toHex(interval[0]);
-      minutes.textContent = toHex(interval[1]);
-      seconds.textContent = toHex(interval[2]);
-   }
-
-
-   function logElapsedTimeHex() {
-
-
-      var currentTime = new Date();
-      var elapsedTime = currentTime - startTime;
-      var elapsed     = millisecondsToInterval(elapsedTime);
-
-      console.log("currentTime: ", startTime);
-
-      var percentMinute = elapsed[2] / 60 * 100;
-
-      var percentWidth = percentMinute.toString(10) + "%";
-      document.querySelector('.timer-bar').style.width = percentWidth;
-
-
-      console.log("percentMinute: ", percentMinute);
-      displayHex(elapsed);
-
-   //   updateBackground(elapsed);
-
-   }
-
-
-
-  function logElapsedTime() {
+  function logTime() {
      var currentTime = new Date();
-     var elapsedTime = currentTime - startTime;
-     var elapsed     = millisecondsToInterval(elapsedTime);
+     console.log("currentTime: ", currentTime);
 
-    console.log("currentTime: ", startTime);
+     var currentHours   = currentTime.getHours();
+     var currentMinutes =  currentTime.getMinutes();
+     var currentSeconds = currentTime.getSeconds();
 
-    var percentMinute = elapsed[2] / 60 * 100;
+     var hexHours   = parseInt(currentHours,10).toString(16);
+     var hexMinutes = parseInt(currentMinutes,10).toString(16);
+     var hexSeconds = parseInt(currentSeconds,10).toString(16);
+
+    var percentMinute = currentSeconds / 60;
     console.log("percentMinute: ", percentMinute);
+    var percentWidth = (percentMinute * 100).toString(10) + "%";
+    document.querySelector('.seconds-bar').style.width = percentWidth;
 
-    var percentWidth = percentMinute.toString(10) + "%";
-    document.querySelector('.timer-bar').style.width = percentWidth;
+   //  var hexColor = "#" + ;
+   //  console.log("hexColor: ", hexColor.toString(16));
 
+    console.log("isHex: ", isHex);
+    if(isHex) {
+       currentHours   = hexHours;
+       currentMinutes = hexMinutes;
+       currentSeconds = hexSeconds;
+    }
 
-    console.log("percentMinute: ", percentMinute);
-    displayInterval(elapsed);
-    updateBackground(elapsed);
+    hours.textContent   = ("0" + currentHours).slice(-2);
+    minutes.textContent = ("0" + currentMinutes).slice(-2);
+    seconds.textContent = ("0" + currentSeconds).slice(-2);
 
+    var hexColorHours   = ("0" + hexHours).slice(-2);
+    var hexColorMinutes = ("0" + hexMinutes).slice(-2);
+    var hexColorSeconds = ("0" + hexSeconds).slice(-2);
 
+    var hexColor = hexColorHours + hexColorMinutes + hexColorSeconds;
 
+    console.log("hexColor: ",hexColor);
+     document.body.style.background = ("#" + hexColor);
   }
 
-  function hoverHex() {
-     clearInterval(timerInterval);
-     hexInterval = window.setInterval(logElapsedTimeHex, 1000);
- }
-
- function clearHex() {
-    clearInterval(hexInterval);
-    timerInterval = window.setInterval(logElapsedTime, 1000);
-}
-
-
-  timer.addEventListener('mouseover', hoverHex);
-  timer.addEventListener('mouseout', clearHex);
+  timer.addEventListener('mouseover', showHex);
+  timer.addEventListener('mouseout', showDecimal);
 
 }());
